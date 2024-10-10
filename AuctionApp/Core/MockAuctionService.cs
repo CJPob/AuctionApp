119c8f7ded7b564072b463499951b7096b48bf27
CJayp <cjaypoblete@hotmail.se>
2024-10-11 01:28:33 +0200

    The details are now sorted by highest bid

--- AuctionApp/Core/MockAuctionService.cs
+++ AuctionApp/Core/MockAuctionService.cs
@@ -30,15 +30,26 @@ namespace AuctionApp.Core
             Auction auction1 = new Auction("Vintage Car", "A rare vintage car", "user@test.com", 5000m, DateTime.Now.AddDays(10));
             Auction auction2 = new Auction("Painting", "Beautiful landscape painting", "user@test.se", 2000m, DateTime.Now.AddDays(5));
 
-            // Användare 2 lägger ett bud på Auktion 1 (Vintage Car)
-            Bid bid1 = new Bid("user@test.se", 5100m);
-            auction1.AddBid("user@test.se", bid1); // Lägg bud för user2 på auction1
+            Bid bid1 = new Bid("user2@test.com", 5100m);  // User2 places a bid
+            Bid bid2 = new Bid("user3@test.com", 5500m);  // Higher bid
+            Bid bid3 = new Bid("user4@test.com", 5000m);  // Equal to opening bid
+            Bid bid4 = new Bid("user5@test.com", 5200m);  // Between previous two bids
 
-            // Användare 1 lägger ett bud på Auktion 2 (Painting)
-            Bid bid2 = new Bid("user@test.com", 2100m);
-            auction2.AddBid("user@test.com", bid2); // Lägg bud för user1 på auction2
+            auction1.AddBid("user2@test.com", bid1); 
+            auction1.AddBid("user3@test.com", bid2);
+            auction1.AddBid("user4@test.com", bid3);
+            auction1.AddBid("user5@test.com", bid4);
 
-            // Lägg till auktionerna i listan över auktioner
+            Bid bid5 = new Bid("user1@test.com", 2100m);  // User1 places a bid
+            Bid bid6 = new Bid("user2@test.com", 2200m);  // Higher bid
+            Bid bid7 = new Bid("user3@test.com", 2050m);  // Lower bid
+            Bid bid8 = new Bid("user4@test.com", 2300m);  // Highest bid
+
+            auction2.AddBid("user1@test.com", bid5);
+            auction2.AddBid("user2@test.com", bid6);
+            auction2.AddBid("user3@test.com", bid7);
+            auction2.AddBid("user4@test.com", bid8);
+
             _auctions.Add(auction1);
             _auctions.Add(auction2);
         }
